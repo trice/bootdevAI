44d1e870dbf65f77bc3881e057bd0001feef02f8
Terry Rice <trice7454@gmail.com>
2025-06-19 18:43:18 -0500

    Refactor content handling in Gemini API integration to support structured message input

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import os
-import sys
+from pyexpat.errors import messages
+
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 from sys import argv
 
 if len(argv) < 2:
     print("Error: no prompt provided")
     exit(1)
-    
+
+user_prompt = argv[1]
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)])
+]    
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
-answer = client.models.generate_content(model="gemini-2.0-flash-001", contents=f"{argv[1]}")
+answer = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 print(answer.text)
 print(f"Prompt tokens: {answer.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {answer.usage_metadata.candidates_token_count}")
