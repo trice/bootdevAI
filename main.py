fd0248b885b2ae900d9cffdc2737f69b8ba36894
Terry Rice <trice7454@gmail.com>
2025-06-19 18:48:25 -0500

    Add verbose mode to main.py for detailed output logging

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from pyexpat.errors import messages
+from tabnanny import verbose
 
 from dotenv import load_dotenv
 from google import genai
@@ -10,6 +11,11 @@ if len(argv) < 2:
     print("Error: no prompt provided")
     exit(1)
 
+verbose = False
+if len(argv) > 2:
+    if argv[2].lower() == "--verbose":
+        verbose = True
+
 user_prompt = argv[1]
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)])
@@ -19,5 +25,8 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 answer = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 print(answer.text)
-print(f"Prompt tokens: {answer.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {answer.usage_metadata.candidates_token_count}")
+
+if verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {answer.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {answer.usage_metadata.candidates_token_count}")
