ee845e68cb054742a18b6d5bec9e8c2121e93e1b
Terry Rice <trice7454@gmail.com>
2025-06-21 18:52:21 -0500

    Added running python scripts

--- tests.py
+++ tests.py
@@ -1,21 +1,24 @@
 import unittest
 
-from functions.write_file import write_file
+from functions.run_python import run_python_file
 
 
-class TestWriteFile(unittest.TestCase):
-    def test_write_file(self):
-        file_content = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-        print(file_content)
-        
-    def test_write_file_to_subdirectory(self):
-        file_content = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-        print(file_content)
-        
-    def test_write_file_to_outside_directory(self):
-        file_content = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-        print(file_content)
+class TestRunFile(unittest.TestCase):
+    def test_with_main(self):
+        result = run_python_file("calculator", "main.py")
+        print(result)
 
+    def test_with_tests(self):
+        result = run_python_file("calculator", "tests.py")
+        print(result)
+
+    def test_with_outside_allowed_directory(self):
+        result = run_python_file("calculator", "../main.py")
+        print(result)
+
+    def test_with_nonexistent_script(self):
+        result = run_python_file("calculator", "nonexistent.py")
+        print(result)
 
 if __name__ == "__main__":
     unittest.main()
